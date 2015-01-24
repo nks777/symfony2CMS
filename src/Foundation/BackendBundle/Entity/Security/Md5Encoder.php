a8a962e080286d323d571e035dc12aec0451eeec
nks <nks@nks-P31-DS3L>
2015-01-24 18:44:37 +0200

    symfony encrypt password

--- src/Foundation/BackendBundle/Entity/Security/Md5Encoder.php
+++ src/Foundation/BackendBundle/Entity/Security/Md5Encoder.php
@@ -6,7 +6,7 @@ use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 
 /**
  * Description of Md5Encoder
- *
+ * UPDATE `symfony`.`user` SET `password`=MD5(CONCAT(LEFT(salt,10),'s^q', 'password')) WHERE id=?;
  * @author nks
  */
 class Md5Encoder implements PasswordEncoderInterface{
@@ -15,7 +15,7 @@ class Md5Encoder implements PasswordEncoderInterface{
     }
     
     public function encodePassword($raw, $salt) {
-        return md5(substr($salt, 0, 10)  . "s" . $raw);
+        return md5(substr($salt, 0, 10)  . "s^q" . $raw);
     }
 
     public function isPasswordValid($encoded, $raw, $salt) {

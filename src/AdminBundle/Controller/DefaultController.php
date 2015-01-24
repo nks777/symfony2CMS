1a1f1a4c5facba86523d11c7600ca0197e6656fa
nks <nks@nks-P31-DS3L>
2015-01-24 18:57:30 +0200

    remove app bundle

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -6,10 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * @Route("/admin")
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/admin")
+     * @Route("/")
      * @Template("AdminBundle:Default:index.html.twig")
      */
     public function indexAction()
